bac2a284cec052a158d7afe8ecda4e6c2dc8932e
JarbeyOcampo1 <JarbeyOcampo1>
2024-03-31 13:29:40 -0500

    primera entrega

---
+++ README.md
@@ -0,0 +1,7 @@
+# 1. Entrega numero 1 del Api
+- Esta Api se trata de una serie de motos
+- Tenemos La Entrega1Application donde se ejecutara el codigo 
+- Construi El modelo usuario el cual contiene: Una ID, Una Marca y Modelo de una moto
+- Tambien esta el controlador que agrega, modifica, busca y elimina motos 
+- Esta el archivo json que lo usamos como base de datos para guardar la informacion de las motos
+
---
+++ src/main/java/com/entrega/entrega1/model/User.java
@@ -0,0 +1,40 @@
+package com.entrega.entrega1.model;
+
+public class User {
+    private Long id;
+    private String modelo;
+    private String marca;
+
+    public User() {
+    }
+
+    public User(Long id, String modelo, String marca) {
+        this.id = id;
+        this.modelo = modelo;
+        this.marca = marca;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setPlaca(Long id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
